1be699d0f6723269ac0dca968187620b72538d71
Jean-Marc KERVIL <jkervil@gmail.com>
2026-03-09 08:42:01 +0100

    Initialisation des fichiers création de classes Lion, Ours, Pierre, Loup dans le .h

---
+++ gameOfLife.h
@@ -0,0 +1,29 @@
+#include <string>
+class Lion 
+{
+    public :
+        void attaque();
+        void deplace(int maxX, int maxY);
+};
+
+
+class Ours 
+{
+    public :
+        void attaque();
+        void deplace(int maxX, int maxY);
+};
+
+class Pierre 
+{
+    public :
+        void attaque();
+        void deplace(int maxX, int maxY);
+};
+
+class Loup 
+{
+    public :
+        void attaque();
+        void deplace(int maxX, int maxY);
+};
